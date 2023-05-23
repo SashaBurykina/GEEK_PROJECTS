1cea9a222fe6f173aa3043a40c175272c27abadd
sashaburykina <dudnika85@gmail.com>
2023-05-23 17:14:21 +0300

    modified  task01/Program.cs

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-Console.Write ("Enter a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-Console.Write ("Enter a number: ");
+Console.Write ("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
-if (num == num1 * num1)
+Console.Write ("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
 {
-    Console.WriteLine($"Да, {num} является квадратом числа {num1}");
+    Console.WriteLine($"Да, {num1} является квадратом числа {num2}");
 }
-else Console.WriteLine($"Нет, {num} не является квадратом числа {num1}");
+else Console.WriteLine($"Нет, {num1} не является квадратом числа {num2}");
