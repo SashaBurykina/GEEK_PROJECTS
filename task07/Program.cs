2f72c871c3af7626cd7118f7c3978f89c9b5b757
sashaburykina <dudnika85@gmail.com>
2023-05-24 16:49:21 +0300

    Added task07/

--- task07/Program.cs
+++ task07/Program.cs
@@ -5,3 +5,15 @@
 // 782 -> 2
 // 918 -> 8
 
+Console.Write("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"{lastDigit} является последней цифрой числа {number}.");
+}
+else
+{
+    Console.WriteLine("Неверное число.");
+}
