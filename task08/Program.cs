5fe8e1e40256ba7fe13056415a1b842a1a774970
sashaburykina <dudnika85@gmail.com>
2023-05-24 17:27:30 +0300

    Added task08/

---
+++ task08/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое, положительное число больше нуля: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    Console.Write($"Вы ввели отрицательное число! ");
+}
+else if (num == 0)
+{
+    Console.Write($"Число ноль не считается ни положительным, ни отрицательным! ");
+}
+else if (num == 1)
+{
+    Console.Write($"Нет чётных чисел в '{num}'! ");
+}
+else
+{
+    int count = 2;
+    Console.Write($"Чётные: {count}");
+    count += 2;
+    while (count <= num)
+    {
+        Console.Write($", {count}");
+        count += 2;
+    }
+}
