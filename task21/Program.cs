f60429aaa797ff96cc52cbd14f39a847a491ad4b
sashaburykina <dudnika85@gmail.com>
2023-05-28 19:53:47 +0300

    Changed task21/

--- task21/Program.cs
+++ task21/Program.cs
@@ -2,3 +2,42 @@
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("Введите координаты двух точек на плоскости: ");
+
+Console.WriteLine("Введите значение X для первой точки: ");
+int x1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y для первой точки: ");
+int y1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Z для первой точки: ");
+int z1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение X для второй точки: ");
+int x2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y для второй точки: ");
+int y2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Z для второй точки: ");
+int z2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+
+double distance = Distance(x1Coordinate, y1Coordinate, z1Coordinate, x2Coordinate, y2Coordinate, z2Coordinate);
+double dRound = Math.Round(distance, 2, MidpointRounding.ToZero);
+
+Console.WriteLine($"Расстояние между заданными точками = {dRound}");
+
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    int x = x2 - x1;
+    int y = y2 - y1;
+    int z = z2 - z1;
+    int sum = x * x + y * y + z * z;
+
+    double result = Math.Sqrt(sum);
+
+    return result;
+}
