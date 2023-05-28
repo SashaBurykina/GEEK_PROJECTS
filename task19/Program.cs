8872c236f9e14c653a2d9096e8f4ca4ff0afa369
sashaburykina <dudnika85@gmail.com>
2023-05-28 19:50:25 +0300

    Changed task19/

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,23 +3,50 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+int FirstDigit(int num)
+{
+    int number = num / 10000;
+    return number;
+}
+
+int SecondDigit(int num)
+
+{
+    int number = num / 1000 % 10;
+    return number;
+}
+
+int FourDigit(int num)
+
+{
+    int number = num / 10 % 10;
+    return number;
+}
+
+int FiveDigit(int num)
+{
+    int number = num % 10;
+    return number;
+}
+
 Console.WriteLine("Введите пятизначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 10000)
+
+if (number > 10000 || number < -10000)
 {
 
 
-    if (FirstDigit == FiveDigit && SecondDigit == FourDigit)
+    if (FirstDigit(number) == FiveDigit(number) && SecondDigit(number) == FourDigit(number))
     {
         Console.WriteLine($"{number} является полиндромом");
     }
     else
-    {   
-            Console.WriteLine($"{number} НЕ является полиндромом");
+    {
+        Console.WriteLine($"{number} НЕ является полиндромом");
     }
 }
 else
-    {
-        Console.WriteLine("Введено неверное число");
-        return;
-    }
+{
+    Console.WriteLine("Введено неверное число");
+    return;
+}
