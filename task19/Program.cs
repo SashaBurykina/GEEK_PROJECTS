d6f25980fbd57c1d1665e78ed5b9c610bf913d50
sashaburykina <dudnika85@gmail.com>
2023-05-28 19:47:21 +0300

    Added task23/

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,3 +3,23 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 10000)
+{
+
+
+    if (FirstDigit == FiveDigit && SecondDigit == FourDigit)
+    {
+        Console.WriteLine($"{number} является полиндромом");
+    }
+    else
+    {   
+            Console.WriteLine($"{number} НЕ является полиндромом");
+    }
+}
+else
+    {
+        Console.WriteLine("Введено неверное число");
+        return;
+    }
---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Write("Введите наутральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+WriteLinesSquare(number);
+
+void WriteLinesSquare(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        int square = i * i *i;
+        Console.WriteLine($"{i,3} -> {square,4}");
+        i++;
+    }
+}
